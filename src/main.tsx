ba8f4d8e38b97e14acd52a4302fc1491475c4771
ExeDevCentral <EXEMETAL@hotmail.com>
2026-05-29 01:22:37 -0300

    chore: add react-query, react-hook-form+zod, date-fns, husky+lint-staged, eslint a11y

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import { SpeedInsights } from '@vercel/speed-insights/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthSessionProvider } from './core/auth/AuthSessionProvider'
+
+const queryClient = new QueryClient()
 import App from './App.tsx'
 import StorePage from './components/store/StorePage'
 import Login from './pages/Login'
@@ -17,21 +20,23 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <AuthSessionProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/tienda" element={<StorePage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/auth/callback" element={<AuthCallback />} />
-            <Route path="/privacidad" element={<PrivacyPolicy />} />
-            <Route path="/terminos" element={<TermsOfService />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthSessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthSessionProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/tienda" element={<StorePage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/auth/callback" element={<AuthCallback />} />
+              <Route path="/privacidad" element={<PrivacyPolicy />} />
+              <Route path="/terminos" element={<TermsOfService />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </AuthSessionProvider>
+      </QueryClientProvider>
       <SpeedInsights />
     </HelmetProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
